9e270054827bcb7843e274a2e91740a42f9401ab
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-11-03 15:02:46 -0300

    Dia 117

--- algorithms/2678NumberOfSeniorCitizens.py
+++ algorithms/2678NumberOfSeniorCitizens.py
@@ -1,16 +1,20 @@
 def countSeniors(details):
 
+    # variaveis
     contador = 0
     string = ""
     idade = 0
+
+    # percorre a lista
     for i in range(len(details)):
+        # transforma a string nos indices 11 e 12 da string atual (idade)
         string = (details[i][11]) + (details[i][12])
+        # transforma a string em int para verificar se é maior que 60
         idade = int(string)
+        # se for maior que 60, incrementa o contador
         if idade > 60:
             contador += 1
         
-
-
 
     return contador
         
