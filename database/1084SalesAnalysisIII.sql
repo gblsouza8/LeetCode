bd04144d405a3ccba7b318b5bf03c391e878c48b
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-12-28 13:36:24 -0300

    Dia 172

---
+++ database/1084SalesAnalysisIII.sql
@@ -0,0 +1,1 @@
+select distinct P.product_id, P.product_name from Product P right join Sales S on P.product_id = S.product_id where P.product_id not in (select product_id from Sales where sale_date > '2019-03-31' or sale_date < '2019-01-01') and S.sale_date between '2019-01-01' and '2019-03-31';
