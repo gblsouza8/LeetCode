72c03f31f34a58e1a2927788551bcd8e989d7e14
Gabriel de Souza <gabrielsouza080802@gmail.com>
2026-01-16 13:54:26 -0300

    Dia 191

--- algorithms/1588SumOfAllOddLengthSubarrays.py
+++ algorithms/1588SumOfAllOddLengthSubarrays.py
@@ -1,13 +1,19 @@
 def sumOddLengthSubarrays(arr):
 
-
+    # variavel que será incrementada
     contador = 0
 
+    # percorre a lista
     for i in range(len(arr)+1):
 
+        # se i for impar
         if i % 2 != 0:
+            # percorre a lista até o i atual
             for j in range(0, len(arr)-i +1):
+                # incrementa contador
                 contador += sum(arr[j:j + i])
+
+    # retorna o contador
     return contador
 
 
