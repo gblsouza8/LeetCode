32ca5d7b1c67670367669dd70ac3db8e10b7cc1c
Gabriel de Souza <gabrielsouza080802@gmail.com>
2026-02-06 16:34:47 -0300

    Dia 212

---
+++ database/1934ConfirmationRate.sql
@@ -0,0 +1,1 @@
+select distinct S.user_id, round(sum(case when action='confirmed' then 1 else 0 end) * 1.0 / count(*), 2) as confirmation_rate from Signups S left join Confirmations C on S.user_id = C.user_id group by S.user_id;
