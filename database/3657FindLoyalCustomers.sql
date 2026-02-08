8ab6ec550505fecfd1ba5c07ee701f1f6473e196
Gabriel de Souza <gabrielsouza080802@gmail.com>
2026-02-08 16:45:00 -0300

    Dia 214

---
+++ database/3657FindLoyalCustomers.sql
@@ -0,0 +1,1 @@
+select customer_id from customer_transactions group by customer_id having sum(case when transaction_type = 'purchase' then 1 else 0 end) >= 3 and sum(case when transaction_type = 'refund' then 1 else 0 end)/sum(case when transaction_type = 'purchase' then 1 else 0 end) <= 0.20 and datediff(max(transaction_date), min(transaction_date)) >= 30;
