97b77822f3832e77ce70cb2cabc44ea10a814757
Gabriel de Souza <gabrielsouza080802@gmail.com>
2026-02-22 16:20:04 -0300

    Dia 228

---
+++ database/1174ImmediateFoodDeliveryII.sql
@@ -0,0 +1,1 @@
+with first_order as (select customer_id, min(order_date) as first_order_date from delivery group by customer_id) select round(sum(case when d.order_date = d.customer_pref_delivery_date then 1 else 0 end) * 100.0 / count(*),2) as immediate_percentage from delivery d join first_order f on d.customer_id = f.customer_id and d.order_date = f.first_order_date;
